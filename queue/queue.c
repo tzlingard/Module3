9f49e1de042b4e74c669ded6cb6b1bf07ffc223b
Vann Guarnieri <Francesco.V.Guarnieri.21@dartmouth.edu>
2020-10-08 19:09:49 -0400

    Added queue search function

--- queue/queue.c
+++ queue/queue.c
@@ -84,6 +84,28 @@ void qapply(queue_t *qp, void (*fn)(void* elementp)){
   }
 }
 
+
+/* search a queue using a supplied boolean function
+ * skeyp -- a key to search for
+ * searchfn -- a function applied to every element of the queue
+ *          -- elementp - a pointer to an element
+ *          -- keyp - the key being searched for (i.e. will be 
+ *             set to skey at each step of the search
+ *          -- returns TRUE or FALSE as defined in bool.h
+ * returns a pointer to an element, or NULL if not found
+ */
+void* qsearch(queue_t *qp, bool (*searchfn)(void* elementp,const void* keyp), const void* skeyp){
+	node_t* n;
+	for (n=((queueStruct_t*)qp)->front; n!=NULL; n=n->next) {
+		if (searchfn(n->element, skeyp)) {
+				return n->element;
+			}
+	}
+	return NULL;
+}
+
+
+
 void qconcat (queue_t *q1p, queue_t *q2p) {
   if ((queueStruct_t*)q1p != NULL && (queueStruct_t*)q2p != NULL) {
     while ((queueStruct_t*)q2p != NULL) {
