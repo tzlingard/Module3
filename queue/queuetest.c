54e5e69a7b529dfa4f12fae1b2606f7965b4bfa5
Vann Guarnieri <Francesco.V.Guarnieri.21@dartmouth.edu>
2020-10-11 12:04:59 -0400

    Cleaned up queuetest.c

--- queue/queuetest.c
+++ queue/queuetest.c
@@ -43,8 +43,10 @@ void print_car(car_t *car) {
 	printf("\n");
 }
 
-bool platecheck(car_t* c, char* platep){
-	return (strcmp(c->plate, platep) == 0);
+bool platecheck(void* c, const void* p){
+	car_t *car = (car_t*)c;
+	char *plate = (char*)p;
+	return (strcmp(car->plate, plate) == 0);
 }
 
 car_t *front = NULL;
@@ -56,25 +58,41 @@ int main(void) {
   car_t *c3 = make_car("SUNGOD", 80080.42, 2020);
   car_t *c4 = make_car("DARTXC", 1769.99, 1769);
 
-	//qopen, qput and qget test
+
+
+	
+	printf("Test 1: Open a new queue\n");
 	queue_t *q1 = qopen();
+	printf("\n\n");
+	
+	printf("Test 2: Get and print from empty queue\n");
+	car_t *cc1 = (car_t*)qget(q1);
+	print_car(cc1);
+	printf("\n\n");
+
+	printf("Test 3 : Put to queue\n");
 	qput(q1,(void*)c1);
 	qput(q1,(void*)c2);
 	qput(q1,(void*)c3);
 	qput(q1,(void*)c4);
-	
-	car_t *cc1 = (car_t*)qget(q1);
+	printf("\n\n");
+
+	printf("Test 4: Get and print from nonempty queue\n");
+	cc1 = (car_t*)qget(q1);
 	print_car(cc1);
+	printf("\n\n");
 
-	//qsearch test
+	printf("Test 5: Search for plate 'SUNGOD' from nonempty queue\n");
 	char* plate1 = "SUNGOD";
-	car_t *find_car1 = qsearch(q1, platecheck, plate1);
-	print_car(find_car1);
+	car_t *find_car1 = qsearch(q1, platecheck, (void*)plate1);
+ 	print_car(find_car1);
+	printf("\n\n");
 
+	printf("Test 6: Search for plate 'THIS_SHOULD_FAIL' from nonempty queue\n");
 	char* plate2 = "THIS_SHOULD_FAIL";
-	car_t *find_car2 = qsearch(q1, platecheck, plate2);
+	car_t *find_car2 = qsearch(q1, platecheck, (void*)plate2);
 	print_car(find_car2);
-
+	printf("\n\n");
 
 	cc1 = (car_t*)qget(q1);
 	cc1 = (car_t*)qget(q1);
